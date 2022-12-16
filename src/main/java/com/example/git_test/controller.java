dbe1903e13746678e676d6d913fac19b5b4acc1e
ocd555 <ocd100@gmail.com>
2022-12-16 15:29:53 +0900

    initial

---
+++ src/main/java/com/example/git_test/controller.java
@@ -0,0 +1,14 @@
+package com.example.git_test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+ @GetMapping("/")
+ public String helloworld() {
+     return "To-do Application !";
+         }
+        }
